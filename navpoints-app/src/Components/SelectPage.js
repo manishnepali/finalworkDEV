7449896d751bacd0cce15e25f5ca7474f0287deb
manishnepali <manish.nepali@student.ehb.be>
2022-06-09 00:46:11 +0200

    select for filter

--- navpoints-app/src/Components/SelectPage.js
+++ navpoints-app/src/Components/SelectPage.js
@@ -1,6 +1,5 @@
 import Start from "./Start";
 import { useState, useEffect} from 'react';
-import data from "../test.json"
 import {
     BrowserRouter as Router,
     Switch,
@@ -9,6 +8,15 @@ import {
   } from "react-router-dom";
 import MapBox from "./Mapbox";
 import Login from "../Backend/LogIn";
+import {db, 
+  collection,
+   getDocs,
+    where, 
+    query, 
+    setDoc,
+     addDoc,
+    doc} from "../Backend/firebase"
+    import Select from 'react-select'
   
 export default function SelectPage() {
   localStorage.setItem("mapsOption", false);
@@ -17,6 +25,7 @@ export default function SelectPage() {
 // selected[0] = prompt("test");
  const backimg = "https://images.unsplash.com/photo-1648737966636-2fc3a5fffc8a?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670";
 function changeColor(e){
+  
   const objSel = e.target.innerText;
     console.log("obs:",objSel) 
   if(bgColor == false){
@@ -26,7 +35,7 @@ function changeColor(e){
    
 
    localStorage.setItem(selectedList, JSON.stringify(objSel));
-  }else if(bgColor == true){
+  }else{
 
     e.target.style.backgroundColor = "#ffffff";
     isBgColor(false)
@@ -43,6 +52,35 @@ function changeColor(e){
   
 // e.target.setBgColor("#BF100080");
 }
+const [data, setData] = useState([]);
+const getData = async() =>{
+  const q = query(collection(db, "event_category"));
+
+  const querySnapshot = await getDocs(q);
+  const dataSet = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id
+  }));
+  setData(dataSet);
+  console.log(dataSet)
+}
+const options = data.map((item, index)=>{
+                return  [{
+                  value: index,
+                  label: item.name
+                }]
+                
+              });
+ 
+
+
+
+
+useEffect(()=>{
+  getData();
+  console.log("event catagory",data);
+
+},[])
 
 const [bgColor, isBgColor] = useState(false);
     return (
@@ -55,26 +93,42 @@ const [bgColor, isBgColor] = useState(false);
             <div className='bg-backimg h-screen md:flex overflow-hidden'>
 
                
-                <div className='container lg:ml-4 md:ml-4 resize-y bg-white box-border h-4/4 relative top-1/4 w-screen pb-36 rounded-t-2xl items-center' >
+                <div className='container lg:ml-4 md:ml-4 resize-y bg-white box-border h-full relative top-1/4 w-screen pb-36 rounded-t-2xl items-center' >
                
                 <Link to="/">
-                <svg class="w-16 h-16  dark:text-rose-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"></path></svg>
+                <svg class="w-16 h-16 mx-8 mt-8  dark:text-rose-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"></path></svg>
                    </Link>
-                     <h1 className="font-bold text-5xl ml-4">
+                     <h1 className="font-bold text-5xl mt-4 ml-8">
                      In mood to:
                     </h1>
              
-                    <ul className="grid grid-cols-2 max-h-60 mt-10 overflow-y-auto ml-4 mr-4">
+                    <ul className="flex flex-wrap max-h-4/5 overflow-x-auto mx-4 mt-4">
+                     {/* <Select 
+                     className="text-4xl text-black font-bold mt-4 ml-4"
+                    
+                     options={options}/> */}
+                      
                     
                       {data.map((category, index)=>{
-                        return <li>
+                        return <li className="p-4">
+                          <input 
+                          onClick={changeColor}
+                          className="h-8 w-8 border border-gray-300 bg-white checked:bg-blue-600 
+                          checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2"
+                          type="radio" id={category.id} name="filter" value={category.name}/>
+                          <label 
+                          className="text-4xl text-black font-bold my-4 ml-4"
                           
-                          <h1 className="text-4xl text-black font-bold mt-4 ml-4"
+                          for={category.id}>{category.name}</label>
+                        
+                          {/* <h1 className="text-4xl text-black font-bold my-4 ml-4"
                            style={{backgroundColor: "#ffffff"}} 
-                           onClick={changeColor} key={index}>{category.name} </h1> </li>
-                      })}
+                           onClick={changeColor} key={index}>{category.name} </h1>
+                      */}
+                       </li>
+                      })} 
                     </ul>
-                    <Link to="/maps"><button class="bg-rose-600 w-2/3 ml-16 text-3xl text-white font-bold py-5 px-6 rounded-full my-8 items-stretch ">
+                    <Link to="/maps"><button class="bg-rose-600 w-2/3 ml-16 text-3xl text-white font-bold py-5 px-6 rounded-full mt-8 items-stretch ">
                   explore
                     </button></Link>
                     
